7e93edc4f6b9f7b953c659076258a05ad326646e
DeveloperAmrit <105799602+DeveloperAmrit@users.noreply.github.com>
2025-08-17 14:15:45 +0530

    Post fixes PR 12 	modified:   components/shared/Navbar.tsx

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -3,7 +3,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import Link from "next/link"
 import { useEffect, useState, FC } from "react"
-import { usePathname } from "next/navigation"
 
 const navLinks = [
   { href: "/", label: "Home" },
