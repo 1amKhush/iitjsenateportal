dcecba6974d9f46d635312eab14497136e2ed116
DeveloperAmrit <105799602+DeveloperAmrit@users.noreply.github.com>
2025-08-21 19:12:08 +0530

    Data heavy - Added all pors to people.js 	modified:   data/people.js

--- data/people.js
+++ data/people.js
@@ -14,10 +14,10 @@ export const people = [
   },
   {
     id: 2,
-    name: "Tharakdatta Hegde",
+    name: "Tharakadatta D Hegde",
     pors: ["General Secretary, Student Senate"],
     category: "gen-sec",
-    email: "gensecy_ss@iitj.ac.in",
+    email: "b22cs102@iitj.ac.in",
     phone: "+91 1234567890",
     links: {
       linkedin: "https://www.linkedin.com/in/tharakdatta-hegde/",
@@ -30,8 +30,8 @@ export const people = [
     name: "Manas Chechani",
     pors: ["General Secretary, SAC"],
     category: "gen-sec",
-    email: "gensecy_sac@iitj.ac.in",
-    phone: "+91 1234567890",
+    email: "b22ai053@iitj.ac.in",
+    phone: "+91 9521771854",
     links: {
       linkedin: "https://www.linkedin.com/in/manas-chechani/",
       instagram: "https://www.instagram.com/manas_chechani/"
@@ -43,8 +43,8 @@ export const people = [
     name: "Krish Teckchandani",
     pors: ["Vice President, BAI"],
     category: "vp",
-    email: "vp_bai@iitj.ac.in",
-    phone: "+91 1234567890",
+    email: "b23cs1092@iitj.ac.in",
+    phone: "+91 9667164576",
     links: {},
     image: "/images/people/default.jpg"
   },
@@ -53,8 +53,8 @@ export const people = [
     name: "Sourav Chahar",
     pors: ["Vice President, BDS"],
     category: "vp",
-    email: "vp_bds@iitj.ac.in",
-    phone: "+91 1234567890",
+    email: "b23ee1071@iitj.ac.in",
+    phone: "+91 8937085795",
     links: {},
     image: "/images/people/default.jpg"
   },
@@ -73,8 +73,8 @@ export const people = [
     name: "Raghuveer Kulkarni",
     pors: ["Vice President, SAA"],
     category: "vp",
-    email: "vp_saa@iitj.ac.in",
-    phone: "+91 1234567890",
+    email: "b23es1020@iitj.ac.in",
+    phone: "+91 7499027852",
     links: {},
     image: "/images/people/default.jpg"
   },
@@ -83,8 +83,8 @@ export const people = [
     name: "Vyom Shah",
     pors: ["Vice President, BIE"],
     category: "vp",
-    email: "vp_bie@iitj.ac.in",
-    phone: "+91 1234567890",
+    email: "b23cm1058@iitj.ac.in",
+    phone: "+91 9998708013",
     links: {},
     image: "/images/people/default.jpg"
   },
@@ -93,8 +93,8 @@ export const people = [
     name: "Sambhav Jha",
     pors: ["Vice President, BCCA"],
     category: "vp",
-    email: "vp_bcca@iitj.ac.in",
-    phone: "+91 1234567890",
+    email: "b23ee1092@iitj.ac.in",
+    phone: "+91 9508798686",
     links: {},
     image: "/images/people/default.jpg"
   },
@@ -103,8 +103,8 @@ export const people = [
     name: "Vaibhav Singh",
     pors: ["Vice President, BAC"],
     category: "vp",
-    email: "vp_bac@iitj.ac.in",
-    phone: "+91 1234567890",
+    email: "b23ee1101@iitj.ac.in",
+    phone: "+91 9599239032",
     links: {},
     image: "/images/people/default.jpg"
   },
@@ -113,8 +113,8 @@ export const people = [
     name: "Sudhanshu Tamhankar",
     pors: ["Vice President, BSS"],
     category: "vp",
-    email: "vp_bss@iitj.ac.in",
-    phone: "+91 1234567890",
+    email: "b23es1032@iitj.ac.in",
+    phone: "+91 6355798073",
     links: {},
     image: "/images/people/default.jpg"
   },
@@ -123,8 +123,8 @@ export const people = [
     name: "Anshit Agarwal",
     pors: ["Vice President, BLA"],
     category: "vp",
-    email: "vp_bla@iitj.ac.in",
-    phone: "+91 1234567890",
+    email: "b23cs1087@iitj.ac.in",
+    phone: "+91 8006337201",
     links: {},
     image: "/images/people/default.jpg"
   },
@@ -138,45 +138,446 @@ export const people = [
     links: {},
     image: "/images/people/default.jpg"
   },
+  // Cultural Club Members
   {
     id: 14,
-    name: "Person A",
-    pors: ["Coordinator, Devlup Labs", "Vice President, BSS"],
-    category: "technical-club",
-    email: "person.a@iitj.ac.in",
+    name: "Aashish Ranjan",
+    pors: ["Secretary, RAW"],
+    category: "cultural-club",
+    club: "RAW",
+    email: "b24edp013@iitj.ac.in",
     phone: "+91 1234567890",
     links: {},
     image: "/images/people/default.jpg"
   },
   {
     id: 15,
-    name: "Person B",
-    pors: ["Coordinator, Robotics Society"],
-    category: "technical-club",
-    club: "Robotics Society",
-    email: "person.b@iitj.ac.in",
+    name: "Ashish Kurrey",
+    pors: ["Joint Secretary, RAW"],
+    category: "cultural-club",
+    club: "RAW",
+    email: "b24edm010@iitj.ac.in",
     phone: "+91 1234567890",
     links: {},
     image: "/images/people/default.jpg"
   },
   {
     id: 16,
-    name: "Person C",
-    pors: ["Coordinator, The Groove Theory"],
+    name: "Nitesh Yadav",
+    pors: ["Joint Secretary, RAW"],
     category: "cultural-club",
-    club: "The Groove Theory",
-    email: "person.c@iitj.ac.in",
+    club: "RAW",
+    email: "b24ci1037@iitj.ac.in",
     phone: "+91 1234567890",
     links: {},
     image: "/images/people/default.jpg"
   },
   {
     id: 17,
-    name: "Person D",
-    pors: ["Coordinator, Cricket Society"],
+    name: "Aditya Rathod",
+    pors: ["Secretary, Designerds"],
+    category: "cultural-club",
+    club: "Designerds",
+    email: "b24ee1062@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 18,
+    name: "Akshat Rathore",
+    pors: ["Joint Secretary, Designerds"],
+    category: "cultural-club",
+    club: "Designerds",
+    email: "b24bb1004@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 19,
+    name: "Harsh Raj",
+    pors: ["Joint Secretary, Designerds"],
+    category: "cultural-club",
+    club: "Designerds",
+    email: "b24es1024@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 20,
+    name: "Soham Khairnar",
+    pors: ["Secretary, Sangam"],
+    category: "cultural-club",
+    club: "Sangam",
+    email: "b23cm1039@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 21,
+    name: "Sumana Nimmagadda",
+    pors: ["Joint Secretary, Sangam"],
+    category: "cultural-club",
+    club: "Sangam",
+    email: "b24cs1049@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 22,
+    name: "Mudit Kumar Singh",
+    pors: ["Joint Secretary, Sangam"],
+    category: "cultural-club",
+    club: "Sangam",
+    email: "b24ci1032@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 23,
+    name: "Arpan Jain",
+    pors: ["Secretary, Quiz Society"],
+    category: "cultural-club",
+    club: "Quiz Society",
+    email: "b23cs1089@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 24,
+    name: "Aaryan Agrawal",
+    pors: ["Secretary, Literature Society"],
+    category: "cultural-club",
+    club: "Literature Society",
+    email: "b23cs1002@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 25,
+    name: "Darshan Prajapati",
+    pors: ["Secretary, PHEME"],
+    category: "cultural-club",
+    club: "PHEME",
+    email: "b23ch1032@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  // Sports Club Members
+  {
+    id: 26,
+    name: "Shiv Shome",
+    pors: ["Secretary, Chess Society"],
+    category: "sports-club",
+    club: "Chess Society",
+    email: "b24es1031@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 27,
+    name: "Manas Methi",
+    pors: ["Secretary, Table Tennis Society"],
+    category: "sports-club",
+    club: "Table Tennis Society",
+    email: "b24me1045@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 28,
+    name: "Aditya Ranjan",
+    pors: ["Coordinator, Football Society"],
+    category: "sports-club",
+    club: "Football Society",
+    email: "b24cm1004@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 29,
+    name: "Manthan Nayak",
+    pors: ["Coordinator, Hockey Society"],
+    category: "sports-club",
+    club: "Hockey Society",
+    email: "b24cm1044@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 30,
+    name: "Mugdha Ghodke",
+    pors: ["Secretary, Badminton Society"],
+    category: "sports-club",
+    club: "Badminton Society",
+    email: "b24ci1033@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 31,
+    name: "Pranjal Shukla",
+    pors: ["Secretary, Basketball Society"],
+    category: "sports-club",
+    club: "Basketball Society",
+    email: "b24ci1045@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 32,
+    name: "Herschelle Sharma",
+    pors: ["Secretary, Athletics Society"],
+    category: "sports-club",
+    club: "Athletics Society",
+    email: "b24edp007@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 33,
+    name: "Krishna Saglani",
+    pors: ["Secretary, Squash Society"],
+    category: "sports-club",
+    club: "Squash Society",
+    email: "b23cs1027@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 34,
+    name: "Raj Khedkar",
+    pors: ["Secretary, Volleyball Society"],
+    category: "sports-club",
+    club: "Volleyball Society",
+    email: "b23ch1038@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 35,
+    name: "Nikhil Yadav",
+    pors: ["Secretary, Cricket Society"],
     category: "sports-club",
     club: "Cricket Society",
-    email: "person.d@iitj.ac.in",
+    email: "b23me1039@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 36,
+    name: "Pratyush Chauhan",
+    pors: ["Secretary, Football Society"],
+    category: "sports-club",
+    club: "Football Society",
+    email: "pratyush@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  // Technical Club Members
+  {
+    id: 37,
+    name: "Sohom Sarkar",
+    pors: ["Overall Coordinator, Robotics Society"],
+    category: "technical-club",
+    club: "Robotics Society",
+    email: "b23ee1099@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 38,
+    name: "Mrudhul Tula",
+    pors: ["Overall Coordinator, Robotics Society"],
+    category: "technical-club",
+    club: "Robotics Society",
+    email: "b23ee1076@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 39,
+    name: "Lakshya Jain",
+    pors: ["Coordinator, Devlup Labs"],
+    category: "technical-club",
+    club: "Devlup Labs",
+    email: "b23cs1032@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 40,
+    name: "Aradhya Mahajan",
+    pors: ["Coordinator, Devlup Labs"],
+    category: "technical-club",
+    club: "Devlup Labs",
+    email: "b23cs1088@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 41,
+    name: "Arun Kumar",
+    pors: ["Coordinator, Devlup Labs"],
+    category: "technical-club",
+    club: "Devlup Labs",
+    email: "b23cs1005@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 42,
+    name: "Sanchit Garg",
+    pors: ["Overall Coordinator, Product Club"],
+    category: "technical-club",
+    club: "Product Club",
+    email: "b23cy1019@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 43,
+    name: "Poojan Patel",
+    pors: ["Overall Coordinator, Product Club"],
+    category: "technical-club",
+    club: "Product Club",
+    email: "b23ee1053@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 44,
+    name: "Pranshu Desai",
+    pors: ["Secretary, Respawn"],
+    category: "technical-club",
+    club: "Respawn",
+    email: "b24cs1057@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 45,
+    name: "Aditya Arya",
+    pors: ["Overall Coordinator, Quant Club"],
+    category: "technical-club",
+    club: "Quant Club",
+    email: "b23ph1001@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 46,
+    name: "Jenish Thakkar",
+    pors: ["Overall Coordinator, Quant Club"],
+    category: "technical-club",
+    club: "Quant Club",
+    email: "b23ee1087@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  // Academic Society Members
+  {
+    id: 47,
+    name: "Md Shaheen Perwez",
+    pors: ["Society Secretary, School of AI and Data Science"],
+    category: "academic-society",
+    club: "School of AI and Data Science",
+    email: "r23ab0004@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 48,
+    name: "Adithya Subhash",
+    pors: ["Secretary, Engineering Science Society"],
+    category: "academic-society",
+    club: "Engineering Science Society",
+    email: "b23es1003@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 49,
+    name: "Arhaan Natekar",
+    pors: ["Secretary, Department of Electrical Engineering"],
+    category: "academic-society",
+    club: "Department of Electrical Engineering",
+    email: "b24ee1091@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 50,
+    name: "Prashant Singh Thakur",
+    pors: ["Society Secretary, Department of Physics"],
+    category: "academic-society",
+    club: "Department of Physics",
+    email: "m24ph1012@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 51,
+    name: "Shivam Kumar",
+    pors: ["Society Secretary, Interdisciplinary Research Program"],
+    category: "academic-society",
+    club: "Interdisciplinary Research Program",
+    email: "m24id1009@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 52,
+    name: "Prakhar Chauhan",
+    pors: ["Society Secretary, School of Bioscience and Bioengineering"],
+    category: "academic-society",
+    club: "School of Bioscience and Bioengineering",
+    email: "b23bb1032@iitj.ac.in",
+    phone: "+91 1234567890",
+    links: {},
+    image: "/images/people/default.jpg"
+  },
+  {
+    id: 53,
+    name: "Divyansh Vyas",
+    pors: ["Secretary, Department of Mechanical Engineering"],
+    category: "academic-society",
+    club: "Department of Mechanical Engineering",
+    email: "b23me1019@iitj.ac.in",
     phone: "+91 1234567890",
     links: {},
     image: "/images/people/default.jpg"
@@ -202,6 +603,14 @@ export const clubMembers = {
   }, []),
   sportsClubs: people.filter(p => p.category === 'sports-club').reduce((acc, curr) => {
       const clubName = curr.club || curr.pors.find(p => p.includes('Coordinator'))?.split(', ')[1] || 'Unknown';
+      if (!acc.find(c => c.clubName === clubName)) {
+          acc.push({ clubName, members: [] });
+      }
+      acc.find(c => c.clubName === clubName)?.members.push(curr);
+      return acc;
+  }, []),
+  academicSocieties: people.filter(p => p.category === 'academic-society').reduce((acc, curr) => {
+      const clubName = curr.club || curr.pors.find(p => p.includes('Secretary'))?.split(', ')[1] || 'Unknown';
       if (!acc.find(c => c.clubName === clubName)) {
           acc.push({ clubName, members: [] });
       }
