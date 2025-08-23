b32f823c08549c9dd8d582cf34d7e37f70b40261
DeveloperAmrit <105799602+DeveloperAmrit@users.noreply.github.com>
2025-08-23 17:32:36 +0530

    Changes in data 	modified:   components/home/Cards.tsx 	modified:   components/home/Hero.tsx

--- components/home/Cards.tsx
+++ components/home/Cards.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { councils } from '@/data/councils';
 import { ACACBoards, SACBoards } from '@/data/boards';
-import { BCCAClubs, BLAClubs, BACClubs, BSSClubs, SAAClubs } from '@/data/clubs';
+import { BCCAClubs, BLAClubs, BACClubs, BSSClubs, SAAClubs, IRteam } from '@/data/clubs';
 import CardGrid from '../shared/CardGrid';
 import InfiniteMarquee from '../shared/Marquee';
 import InfoCard from '../shared/InfoCard';
@@ -30,6 +30,7 @@ const Cards: React.FC = () => {
   const acacClubs = [
     ...mapItemsForMarquee(BCCAClubs),
     ...mapItemsForMarquee(SAAClubs),
+    ...mapItemsForMarquee(IRteam),
   ];
   const sacClubs = [
     ...mapItemsForMarquee(BLAClubs),
